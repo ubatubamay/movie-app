41b34a61170d207f26bf939db34f9100cdca9173
Mayra <may.ubatuba@gmail.com>
2018-11-05 22:06:06 -0200

    Adicionando configurações do banco

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/movie-app';
+const URI = 'mongodb://ubatubamay:unicornio8!@ds151943.mlab.com:51943/heroku_4c8p1rnr';
 
 mongoose.connect(URI, {useNewUrlParser: true} )
     .then(db => console.log('DB is connected'))
